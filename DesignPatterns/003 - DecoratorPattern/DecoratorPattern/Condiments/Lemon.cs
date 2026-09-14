6907fb89376101bb0350b434e72069a1acdb9de2
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2026-09-14 15:29:12 +0200

    All the drinks from the assignment list are now being displayed.

---
+++ DesignPatterns/003 - DecoratorPattern/DecoratorPattern/Condiments/Lemon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecoratorPattern.Beverages;
+
+namespace DecoratorPattern.Condiments
+{
+    internal class Lemon : CondimentDecorator
+    {
+        private Beverage _beverage;
+
+        public Lemon(Beverage beverage)
+        {
+            _beverage = beverage;
+        }
+
+        public override string GetDescription()
+        {
+            return _beverage.GetDescription() + ", Lemon";
+        }
+
+        public override double cost()
+        {
+            return _beverage.cost() + 0.15;
+        }
+    }
+}
